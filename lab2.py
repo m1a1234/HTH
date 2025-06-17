b3a68aeaa5e29feb294be9f571f5a14885d04ba0
Mia Carozza <mialunacarozza@gmail.com>
2025-06-17 12:12:02 -0700

    Lab2

---
+++ lab2.py
@@ -0,0 +1,14 @@
+# Lab 2
+
+name = "Mia Carozza"
+
+nickname = "Mia"
+
+age = 16
+print(age)
+
+used_python = False
+
+hobbies = ["sports" , "eating" , "sleeping"]
+
+favorite_things = {"sports": "Volleyball, Basketball and Track", "food": "Pho","bed": "My bed"}
