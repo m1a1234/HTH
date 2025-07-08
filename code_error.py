8289a42761d79f08ca7403c3cc22010e696070c0
Mia Carozza <mialunacarozza@gmail.com>
2025-07-08 11:10:45 -0700

    code_error

---
+++ code_error.py
@@ -0,0 +1,42 @@
+#Code Snippet 1:
+# Incorrect:
+x = 10
+y = 2
+result = x / y
+print("Result:", result)
+
+#Code Snippet 2:
+# Incorrect:
+numbers = [1, 2, 3, 4, 5]
+for i in range(len(numbers) - 1):
+   print(numbers[i+1])
+
+
+#Code Snippet 3:
+# Incorrect:
+def calculate_area(radius):
+   area = 3.14 * radius ** 2
+   return area
+ 
+radius = 5
+print(calculate_area(radius))
+
+
+
+#Code Snippet 4:
+# Incorrect:
+def is_even(number):
+   if number % 2 == 0 :
+       return True
+   else:
+       return False
+ 
+print(is_even(4))
+print(is_even(7))
+
+
+
+
+
+
+
